c4281932917e5a6a65c5116f95e2d011ecf24031
Jkorf <jankorf91@gmail.com>
2025-04-22 10:15:17 +0200

    Fixed test

--- Bitget.Net.UnitTests/BitgetSocketIntegrationTests.cs
+++ Bitget.Net.UnitTests/BitgetSocketIntegrationTests.cs
@@ -23,12 +23,13 @@ namespace Bitget.Net.UnitTests
         {
             var key = Environment.GetEnvironmentVariable("APIKEY");
             var sec = Environment.GetEnvironmentVariable("APISECRET");
+            var pass = Environment.GetEnvironmentVariable("APIPASS");
 
             Authenticated = key != null && sec != null;
             return new BitgetSocketClient(Options.Create(new BitgetSocketOptions
             {
                 OutputOriginalData = true,
-                ApiCredentials = Authenticated ? new CryptoExchange.Net.Authentication.ApiCredentials(key, sec) : null
+                ApiCredentials = Authenticated ? new CryptoExchange.Net.Authentication.ApiCredentials(key, sec, pass) : null
             }), loggerFactory);
         }
 
