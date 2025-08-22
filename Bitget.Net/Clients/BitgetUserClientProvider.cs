71633b69c460c4162bdd3a324d01f9b4e2d6a5d3
Jkorf <jankorf91@gmail.com>
2025-08-22 14:54:08 +0200

    Added ClearUserClients method to user client provider

--- Bitget.Net/Clients/BitgetUserClientProvider.cs
+++ Bitget.Net/Clients/BitgetUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace Bitget.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public IBitgetRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, BitgetEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))
