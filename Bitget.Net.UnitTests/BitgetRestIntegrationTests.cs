fe38d9df0a43961312fa245256d9792c3cc1079a
Jkorf <jankorf91@gmail.com>
2025-12-16 10:54:41 +0100

    wip

--- Bitget.Net.UnitTests/BitgetRestIntegrationTests.cs
+++ Bitget.Net.UnitTests/BitgetRestIntegrationTests.cs
@@ -14,7 +14,7 @@ namespace Bitget.Net.UnitTests
     [NonParallelizable]
     internal class BitgetRestIntegrationTests : RestIntegrationTest<BitgetRestClient>
     {
-        public override bool Run { get; set; } = true;
+        public override bool Run { get; set; } = false;
 
         public BitgetRestIntegrationTests()
         {
