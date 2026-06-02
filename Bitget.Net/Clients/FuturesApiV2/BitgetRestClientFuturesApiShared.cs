55ec6c5b6143653025d421ed58ff86be16857fe7
Jkorf <jankorf91@gmail.com>
2026-06-02 15:38:05 +0200

    Fixed Shared GetKlines implementations using incorrect limit default

--- Bitget.Net/Clients/FuturesApiV2/BitgetRestClientFuturesApiShared.cs
+++ Bitget.Net/Clients/FuturesApiV2/BitgetRestClientFuturesApiShared.cs
@@ -450,7 +450,7 @@ namespace Bitget.Net.Clients.FuturesApiV2
 
             var direction = DataDirection.Descending;
             var symbol = request.Symbol!.GetSymbol(FormatSymbol);
-            var limit = request.Limit ?? 1000;
+            var limit = request.Limit ?? 200;
             var pageParams = Pagination.GetPaginationParameters(direction, limit, request.StartTime, request.EndTime ?? DateTime.UtcNow, pageRequest, true);
 
             var result = await ExchangeData.GetHistoricalMarkPriceKlinesAsync(
@@ -513,7 +513,7 @@ namespace Bitget.Net.Clients.FuturesApiV2
 
             var direction = DataDirection.Descending;
             var symbol = request.Symbol!.GetSymbol(FormatSymbol);
-            var limit = request.Limit ?? 1000;
+            var limit = request.Limit ?? 200;
             var pageParams = Pagination.GetPaginationParameters(direction, limit, request.StartTime, request.EndTime ?? DateTime.UtcNow, pageRequest, true);
 
             var result = await ExchangeData.GetHistoricalIndexPriceKlinesAsync(
