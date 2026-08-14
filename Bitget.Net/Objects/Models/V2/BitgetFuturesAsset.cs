3df8bca08e37b3990a84d7531e72aef356f7d26a
Jan Korf <jankorf91@gmail.com>
2026-08-14 09:35:17 +0200

    Rename property 'Coin' to 'Asset' in BitgetFuturesAsset

--- Bitget.Net/Objects/Models/V2/BitgetFuturesAsset.cs
+++ Bitget.Net/Objects/Models/V2/BitgetFuturesAsset.cs
@@ -13,7 +13,7 @@ namespace Bitget.Net.Objects.Models.V2
         /// Asset Name
         /// </summary>
         [JsonPropertyName("coin")]
-        public string Coin { get; set; } = string.Empty;
+        public string Asset { get; set; } = string.Empty;
         /// <summary>
         /// Available
         /// </summary>
